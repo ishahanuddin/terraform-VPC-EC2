ce2ac8b850da042cba83bfa23e262349a95331c0
AzureAD\ShahanUddin <suddin@cinnova.com>
2024-01-13 01:22:34 +0500

    Added S3 bucket for state handling

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.16.1"
     }
   }
+  backend "s3" {
+    bucket         	   = "terraform-assignment-s3-state"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "eu-west-1"
+    encrypt        	   = true
+    dynamodb_table = "tf_lockID"
+  }
 }
 provider "aws" {
     region = "eu-west-1"
